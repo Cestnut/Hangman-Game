7eaf6b644a79cb4ebceb3c1b817ea234194742dd
Cestnut <hunterkudo@gmail.com>
2022-12-31 14:51:17 +0100

    login page

---
+++ www/backend/login.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT']."/backend/settings.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/backend/conn.php");
+
+if(isset($_POST)){
+    try{
+        $username = $_POST["username"];
+        $password1 = $_POST["password"];
+        
+        $stmt = $conn->prepare("SELECT * FROM user WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        
+        $row = $stmt->get_result()->fetch_assoc();
+
+        $password2 = $row['password'];
+
+        if(password_verify($password1, $password2)){
+            $status = "success";
+            $_SESSION['userID'] = $row['ID_user'];
+            $_SESSION['role'] = "user";
+        }
+        else{
+            $status = "wrong_credentials";
+        }
+    }
+    catch(Exception $e){
+        $status = "error";
+    }
+
+    echo $status;
+}
+
+?>
---
+++ www/javascript/login.js
@@ -0,0 +1,39 @@
+window.onload = init
+
+function init(){
+    $("#loginButton").on("click", login);
+}
+
+function login(){
+
+    let form = $("#loginForm");
+
+
+    let username = form.find("[name='username']").val(); 
+    let password = form.find("[name='password']").val(); 
+    console.log(password);
+    $.ajax({
+        url: "../backend/login.php",
+        method: "post",
+        data:{
+            username:username,
+            password:password,
+        }
+      }).done(function(message) {
+            if(message == "success"){
+                window.location = "../html/userHome.html";
+            }
+            else if(message == "wrong_credentials"){
+                errorLogin("username o password errati");
+            }
+            else if(message == "error"){
+                errorLogin("Errore del server");
+            }
+            console.log(message);
+
+        });
+}
+
+function errorLogin(message){
+    $("#errorLogin").html(message);
+}
