fd114af8595ad2c5fa685de956c7e4adec53f491
Cestnut <hunterkudo@gmail.com>
2023-01-02 18:59:19 +0100

    fixed

--- www/javascript/signup.js
+++ www/javascript/signup.js
@@ -8,23 +8,17 @@ function signup(){
 
     let form = $("#signupForm");
 
-    let name = form.find("[name='name']").val(); 
-    let surname = form.find("[name='surname']").val(); 
     let username = form.find("[name='username']").val(); 
     let password = form.find("[name='password']").val(); 
     let confirmPassword = form.find("[name='confirmPassword']").val(); 
-    let voucher = form.find("[name='voucher']").val(); 
 
     $.ajax({
         url: "../backend/signup.php",
         method: "post",
         data:{
-            name:name,
-            surname:surname,
             username:username,
             password:password,
             confirmPassword:confirmPassword,
-            voucher:voucher
         }
       }).done(function(message) {
             console.log(message);
@@ -35,7 +29,7 @@ function signup(){
             else{
                 let errors = message.errors;
 
-                fields = ["name", "surname", "username", "password", "voucher"];
+                fields = ["username", "password"];
                 fields.forEach(function(field) {
                     let errorsField = document.getElementById(field+"Errors");
                     let newErrorsDiv = document.createElement("div");
