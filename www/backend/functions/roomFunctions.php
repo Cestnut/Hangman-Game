135a3f91e7982902bdb12ef8dce5eee08f503297
Cestnut <hunterkudo@gmail.com>
2023-01-07 17:16:27 +0100

    added utility functions

--- www/backend/functions/roomFunctions.php
+++ www/backend/functions/roomFunctions.php
@@ -3,9 +3,6 @@
 require_once($_SERVER['DOCUMENT_ROOT']."/backend/settings.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/backend/conn.php");
 
-function isRoomOpen($room_id){
-}
-
 #Doesn't check if room is open. Used for the first join.
 function joinRoom($room_id, $user_id){ 
     global $conn;
@@ -13,5 +10,47 @@ function joinRoom($room_id, $user_id){
     $stmt = $conn->prepare("INSERT INTO room_partecipation (ID_room, ID_user) VALUES (?,?)");
     $stmt->bind_param("ss", $room_id, $user_id);
     $stmt->execute();
+}
+
+function leaveRoom($room_id, $user_id){ 
+    global $conn;
+
+    $stmt = $conn->prepare("DELETE FROM room_partecipation WHERE ID_room = ? AND  ID_user = ?");
+    $stmt->bind_param("ss", $room_id, $user_id);
+    $stmt->execute();
+}
+
+function isRoomOpen($room_id){
+    global $conn;
+    $sql = "SELECT room.ID_host, room_partecipation.ID_room FROM room 
+            JOIN room_partecipation 
+            ON room.ID_host = room_partecipation.ID_user 
+            AND room.ID_room = room_partecipation.ID_room 
+            WHERE room_partecipation.ID_room = ?";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $room_id);
+    $stmt->execute();
+
+    $stmt->store_result();
+    return $stmt->num_rows == 1;
+}
+
+function isRoomActive($room_id){
+    //TOADD
+    return 1;                             
+}
+
+function isUserInRoom($user_id, $room_id){
+    global $conn;
+    $sql = "SELECT * FROM room_partecipation 
+            WHERE ID_user = ? AND ID_room = ?";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $user_id, $room_id);
+    $stmt->execute();
+
+    $stmt->store_result();
+    return $stmt->num_rows == 1;
 }
 ?>
