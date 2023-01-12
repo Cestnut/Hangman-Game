92b4f1043cf4059f01abcdeeb21643847b9c41c6
Cestnut <hunterkudo@gmail.com>
2023-01-12 17:34:01 +0100

    now escapes sent messages

--- www/backend/sendMessage.php
+++ www/backend/sendMessage.php
@@ -12,11 +12,9 @@ require_once($_SERVER['DOCUMENT_ROOT']."/backend/functions/roomFunctions.php");
 
     if($roomID != '' && $message != '' && isset($_SESSION)){
         if (isRoomOpen($roomID) && isUserInRoom($userID, $roomID)){
-            echo "ciaooo";
             $stmt = $conn->prepare("INSERT INTO message (message, ID_user, ID_room) VALUES (?,?,?)");
-            $stmt->bind_param("sss", $message, $userID, $roomID);
+            $stmt->bind_param("sss", htmlspecialchars($message), $userID, $roomID);
             $stmt->execute();
-            echo $conn->insert_id;
         }
     }
 ?>
