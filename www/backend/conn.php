38d33d10924c0ec8d1e18d5628e06ec096e4a4f7
Cestnut <hunterkudo@gmail.com>
2023-01-18 00:56:24 +0100

    Added comments

--- www/backend/conn.php
+++ www/backend/conn.php
@@ -11,10 +11,10 @@ if ($conn->connect_error){
     die("Errore durante la connessione col database");
 }
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //per far vedere gli errori delle funzioni che riguardano il database
 $conn->set_charset("utf8mb4");
 
-define('ER_DUP_KEY', 1062);
+define('ER_DUP_KEY', 1062); //costante che indica l'errore di chiave duplicata
 
 //unsetting because this script will be included in almost every other script
 unset($servername);
@@ -22,5 +22,5 @@ unset($username);
 unset($password);
 unset($database);
 
-session_start();
+session_start(); //inizializza la variabile _SESSION
 ?>
--- www/backend/settings.php
+++ www/backend/settings.php
@@ -1,7 +1,8 @@
 <?php
-
+//funzioni usate per rendere visibili gli errori
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
-set_time_limit(0);
+
+set_time_limit(0); //necessario per gli script della stanza, della chat e della partita, altrimenti si chiuderebbe il server dopo un tot di secondi. 
 
 ?>
