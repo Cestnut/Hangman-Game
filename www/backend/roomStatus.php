9def133a831595605f8edfcb9fb66b9c4fec5fb3
Cestnut <hunterkudo@gmail.com>
2023-01-18 00:57:04 +0100

    refactored code

--- www/backend/roomStatus.php
+++ www/backend/roomStatus.php
@@ -8,36 +8,63 @@ require_once($_SERVER['DOCUMENT_ROOT']."/backend/functions/gameFunctions.php");
 $roomID = $_GET['roomID'];
 $userID = $_SESSION['userID'];
 session_write_close(); //NECESSARY so other scripts using this same sessione can be executed and don't have to wait until this closes.
+ignore_user_abort(true); //NECESSARY so in case the user is inactive, the destructor functions will be executed
 
 if (isUserInRoom($userID, $roomID)){
-    
     header('Content-Type: text/event-stream');
     header('Cache-Control: no-cache');
     
-    define('HEARTBEAT_PERIOD', 600); //after how many seconds send an heartbeat signal
+    define('HEARTBEAT_PERIOD', 1000); //after how many seconds send an heartbeat signal
     $heartbeatTime = time();
 
     $roomName = "";
     $startEventSent = 0;
-    while(1){
-        if (!(isRoomOpen($roomID)) || connection_aborted() || !(isUserInRoom($userID, $roomID))){ //returns true only if server knows the client disconnected. Namely after a message was sent and the client didn't answer.
-            echo "event: closed\n"; //is needed only in case the room was closed
-            echo "data:\n\n";
-            leaveRoom($roomID, $userID);
-            break; 
+    $loop = true;
+    while($loop){
+        if(checkUserDisconnection($roomID, $userID)){
+            $loop = false;
+        }
+        startEvent($gameID, $roomID, $userID);
+        newNameEvent($roomID);
+        if(checkTimeout($roomID, $userID)){
+            $loop = false;
         }
 
-        if($gameID = isRoomActive($roomID)){
-            if(!$startEventSent && isUserInGame($userID, $gameID)){
-                $startEventSent = 1;
-                echo "event: start\n";
-                echo "data:".$gameID."\n\n";
-            }
+        ob_flush(); //Necessary to send data to the php buffer ready to send. If this wasn't used, no data would arrive to client until the script stopped
+        flush(); //Flushes content from php buffer to client
+        sleep(1);
+    }
+}
+
+function checkUserDisconnection($roomID, $userID){
+
+    if (!(isRoomOpen($roomID)) || !(isUserInRoom($userID, $roomID))){
+        echo "event: closed\n"; //is needed only in case the room was closed
+        echo "data:\n\n";
+        leaveRoom($roomID, $userID);
+        return true;
+    }
+}
+
+function startEvent($gameID, $roomID, $userID){
+    global $startEventSent;
+
+    if($gameID = isRoomActive($roomID)){
+        if(!$startEventSent && isUserInGame($userID, $gameID)){
+            $startEventSent = 1;
+            echo "event: start\n";
+            echo "data:".$gameID."\n\n";
         }
-        else{
-            $startEventSent = 0;
-        }
-        
+    }
+    else{
+        $startEventSent = 0;
+    }
+}
+
+function newNameEvent($roomID){
+        global $conn;
+        global $roomName;
+
         $stmt = $conn->prepare("SELECT name FROM room WHERE ID_room = ?");
         $stmt->bind_param("s", $roomID);
         $stmt->execute();
@@ -48,16 +75,19 @@ if (isUserInRoom($userID, $roomID)){
             echo "event: newName\n";
             echo 'data: ' .$roomName. "\n\n";
         }
-
-        if(time() > $heartbeatTime + HEARTBEAT_PERIOD){
-            echo ": heartbeat\n\n"; //Used by the webserver to know if the connection was closed by the client, in case new messages are never found
-            $heartbeatTime = time();
-        }
-
-        ob_flush(); //Necessary to send data to the php buffer ready to send. If this wasn't used, no data would arrive to client until the script stopped
-        flush(); //Flushes content from php buffer to client
-        sleep(1);
-    }
 }
 
+function checkTimeout($roomID, $userID){
+    global $heartbeatTime;
+    if(time() > $heartbeatTime + HEARTBEAT_PERIOD){
+        echo "data: heartbeat\n\n"; //Used by the webserver to know if the connection was closed by the client, in case new messages are never found
+        $heartbeatTime = time();
+        if(connection_aborted()){ //returns true if the server detected client disconnection. Namely after a message was sent and the client didn't acknowledge.
+            echo "event: timeout\n";
+            echo "data:\n\n";
+            leaveRoom($roomID, $userID);
+            return true;
+        }
+    }
+}
 ?>
